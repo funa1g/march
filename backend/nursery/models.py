795b0b5540fd4061a3f8776a63a38637b440a4a9
funa1g <funa1g@gmail.com>
2017-12-02 22:55:50 +0900

    Modelを変更

--- backend/nursery/models.py
+++ backend/nursery/models.py
@@ -4,11 +4,20 @@ from django.db import models
 class City(models.Model):
     name = models.TextField()
     sort = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Category(models.Model):
+    city = models.ForeignKey(City)
+    text = models.TextField()
+    sort = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
 
 
 class Question(models.Model):
-    question = models.TextField()
-    score = models.IntegerField()
-    city = models.ForeignKey(City)
+    text = models.TextField()
+    score = models.IntegerField(null=True)
+    category = models.ForeignKey(Category, null=True)
+    parent = models.ForeignKey('self', null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     sort = models.IntegerField()
